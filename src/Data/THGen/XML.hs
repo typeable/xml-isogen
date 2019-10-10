ce1fd04d7f4713b898ea6781a0a5a068f6bb6f4d
Denis Redozubov <denis.redozubov@gmail.com>
2019-10-10 12:13:24 +0300

    Export Internal structures for code generation in xsd-isogen

--- src/Data/THGen/XML.hs
+++ src/Data/THGen/XML.hs
@@ -131,61 +131,17 @@ import           Data.Maybe (maybeToList, mapMaybe)
 import           Data.String
 import           Data.THGen.Compat
 import           Data.THGen.Enum
+import           Data.THGen.XML.Internal
 import qualified Data.Text as T
 import           GHC.Generics (Generic)
 import qualified Language.Haskell.TH as TH
 import           Prelude hiding ((+), (*), (^))
 import qualified Text.XML as X
 import           Text.XML.DOM.Parser hiding (parseContent)
+import Text.XML.DOM.Parser.Internal.Content
 import           Text.XML.ParentAttributes
 import qualified Text.XML.Writer as XW
-import Text.XML.DOM.Parser.Internal.Content
 
-
-data XmlFieldPlural
-  = XmlFieldPluralMandatory  -- Occurs exactly 1 time (Identity)
-  | XmlFieldPluralOptional   -- Occurs 0 or 1 times (Maybe)
-  | XmlFieldPluralRepeated   -- Occurs 0 or more times (List)
-  | XmlFieldPluralMultiplied -- Occurs 1 or more times (NonEmpty)
-
-data XmlAttributePlural
-  = XmlAttributePluralMandatory -- Occurs exactly 1 time (Identity)
-  | XmlAttributePluralOptional  -- Occurs 0 or 1 times (Maybe)
-
-data PrefixName = PrefixName String String
-
-data IsoXmlDescPreField = IsoXmlDescPreField String TH.TypeQ
-
-data IsoXmlDescPreAttribute = IsoXmlDescPreAttribute String TH.TypeQ
-
-data IsoXmlDescPreContent = IsoXmlDescPreContent String TH.TypeQ
-
-data IsoXmlDescField = IsoXmlDescField XmlFieldPlural String TH.TypeQ
-
-data IsoXmlDescAttribute = IsoXmlDescAttribute XmlAttributePlural String TH.TypeQ
-
-data IsoXmlDescContent = IsoXmlDescContent String TH.TypeQ
-
-data IsoXmlDescRecordPart
-  = IsoXmlDescRecordField IsoXmlDescField
-  | IsoXmlDescRecordAttribute IsoXmlDescAttribute
-  | IsoXmlDescRecordContent IsoXmlDescContent
-
-newtype IsoXmlDescRecord = IsoXmlDescRecord [IsoXmlDescRecordPart]
-
-makePrisms ''IsoXmlDescRecord
-
-data ExhaustivenessName = ExhaustivenessName String Exhaustiveness
-
-newtype IsoXmlDescEnumCon
-  = IsoXmlDescEnumCon { unIsoXmlDescEnumCon :: String }
-
-instance IsString IsoXmlDescEnumCon where
-  fromString = IsoXmlDescEnumCon
-
-data IsoXmlDescEnum = IsoXmlDescEnum [IsoXmlDescEnumCon]
-
-makePrisms ''IsoXmlDescEnum
 
 appendField
   :: XmlFieldPlural

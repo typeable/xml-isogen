979bf86379012c31204366a06606ecc9f714a269
Evgenii Akentev <i@ak3n.com>
2020-09-03 16:25:10 +0500

    Add shell.nix and info how to use it

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = import ./default.nix;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
